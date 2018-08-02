38b04a725dd9319c35c0e467aed7c8450f520a60
Michel Ribeiro <michelribeiro2004@gmail.com>
2018-08-02 16:12:22 -0300

    Update conversor.js

--- conversor.js
+++ conversor.js
@@ -1,1 +1,10 @@
 // conversor
+
+// convert timestamp in normal date
+let dateInTimeStamp = 1534215300;
+let converterTimeStampToNormalDate = new Date(dateInTimeStamp * 1000);
+console.log(converterTimeStampToNormalDate);
+
+// convert date in timestamp
+let dateNormal = new Date();
+let converterNormalDateToTimeStamp = new Date(dateNormal).getTime() / 1000;
